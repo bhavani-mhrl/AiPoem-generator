00fe806f749cb3d4cb0061c15d76fa5d0682ff85
bhavani-mhrl <mchrlbhavani@gmail.com>
2026-06-07 05:29:27 +0530

    improved user experience and removed console.logs

--- src/index.js
+++ src/index.js
@@ -7,19 +7,19 @@ function displayPoem(response){
     });
 }
 
-function generatePOem(e){
+function generatePoem(e){
     e.preventDefault();
     let searchInput=document.querySelector("#search-input");
+    let poemContainer = document.querySelector("#poem");
+    poemContainer.classList.remove("hidden");
+    poemContainer.innerHTML=`<div class="generating">⌛Generating poem about ${searchInput.value}</div>`;
+
     let apiKey="fa802d0et31047o097e3a46943abb4fe";
     let prompt=`Generate a french poem for ${searchInput.value}`;
-    let context="Assume that your a poem writer and you love to write poems,generate a poem like your a romantic poem writer .just write a 4 lines of short poem.seperate each line with <br/> tag. use sign as 'Your Love' at the end , use <strong> tag and text color same as the rgb(42, 124, 165) for only the sign and dont generate any title,";
+    let context="Assume that your a poem writer and you love to write poems,generate a poem like your a romantic poem writer .just write a 4 lines of short poem.seperate each line with <br/> tag. use the sign as 'Your Love' at the end for every poem, use <strong> tag and text color same as the rgb(42, 124, 165) for only the sign and dont generate any title,";
     let apiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-    console.log("generating poem");
-    console.log("Prompt : ",prompt);
-    console.log("context : ",context);
-
     axios.get(apiUrl).then(displayPoem);
 }
 
 let poemForm = document.querySelector("#poem-form");
-poemForm.addEventListener("submit",generatePOem);
+poemForm.addEventListener("submit",generatePoem);
