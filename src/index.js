8444b89f1e6c7841e4581fe6d5774e655489ca87
bhavani-mhrl <mchrlbhavani@gmail.com>
2026-06-07 05:45:49 +0530

    created a motivation generator

--- src/index.js
+++ src/index.js
@@ -12,11 +12,11 @@ function generatePoem(e){
     let searchInput=document.querySelector("#search-input");
     let poemContainer = document.querySelector("#poem");
     poemContainer.classList.remove("hidden");
-    poemContainer.innerHTML=`<div class="generating">⌛Generating poem about ${searchInput.value}</div>`;
+    poemContainer.innerHTML=`<div class="generating">⌛Generating Motivation your looking ${searchInput.value}</div>`;
 
     let apiKey="fa802d0et31047o097e3a46943abb4fe";
-    let prompt=`Generate a french poem for ${searchInput.value}`;
-    let context="Assume that your a poem writer and you love to write poems,generate a poem like your a romantic poem writer .just write a 4 lines of short poem.seperate each line with <br/> tag. use the sign as 'Your Love' at the end for every poem, use <strong> tag and text color same as the rgb(42, 124, 165) for only the sign and dont generate any title,";
+    let prompt=`Generate a motivation, now i am having ${searchInput.value} mood`;
+    let context="Assume that your a motivator  and you love to Motivate people,generate a motivation that gives confidence , hope and belief.just write a 2 lines of short motivation.seperate each line with <br/> tag. use the sign as 'Your Love' at the end for every poem, use <strong> tag and text color same as the rgb(42, 124, 165) for only the sign and dont generate any title,";
     let apiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
     axios.get(apiUrl).then(displayPoem);
 }
